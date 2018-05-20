6c3b503946cdad34bf7d5aca48dad9120159f72d
winsuperstar <867100248@qq.com>
2018-05-20 16:28:52 +0800

    fix conflicts

--- src/main/java/com/joshua/easypass/repository/RoleRepository.java
+++ src/main/java/com/joshua/easypass/repository/RoleRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.joshua.easypass.entity.Role;
+
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
   
