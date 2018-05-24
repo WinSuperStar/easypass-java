e8ff7fa4a6c59847ec0d3b3e621942e5f59a812f
ChewApple <welikebball@gmail.com>
2018-05-24 23:00:36 +0800

    commit

--- src/main/java/com/joshua/easypass/repository/CustomerRepository.java
+++ src/main/java/com/joshua/easypass/repository/CustomerRepository.java
@@ -12,6 +12,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query("SELECT c FROM Customer c WHERE c.cusid = :cusid")
     public Customer getCustomer(@Param("cusid") Integer cusid);
 
-    @Query("SELECT c FROM Customer c WHERE c.cusname like %:cusname% AND c.cusmode like %:cusmode% AND c.contact like %:contact% AND c.contactPhone like %:contactPhone%")
-    public Customer[] getCustomers(@Param("cusname") String cusname, @Param("cusmode") String cusmode, @Param("contact") String contact, @Param("contactPhone") String contactPhone);
+    @Query("SELECT c FROM Customer c WHERE c.cusname like %:cusname% AND c.cusmode like %:cusmode% AND c.contact like %:contact% AND c.contactPhone like %:contactPhone% AND c.state like %:state%")
+    public Customer[] getCustomers(@Param("cusname") String cusname, @Param("cusmode") String cusmode, @Param("contact") String contact, @Param("contactPhone") String contactPhone, @Param("state") String state);
 }
--- src/main/java/com/joshua/easypass/service/VendorService.java
+++ src/main/java/com/joshua/easypass/service/VendorService.java
@@ -1,12 +1,11 @@
 package com.joshua.easypass.service;
 
-import com.joshua.easypass.entity.Authlist;
-import com.joshua.easypass.entity.User;
 import com.joshua.easypass.entity.Vendor;
-import com.joshua.easypass.repository.AuthlistRepository;
 import com.joshua.easypass.repository.VendorRepository;
-import com.joshua.easypass.util.DataUtil;
 
+import com.joshua.easypass.util.DataUtil;
+import com.joshua.easypass.util.DateUtil;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +13,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -26,24 +31,99 @@ public class VendorService {
     @Autowired
     private VendorRepository vdrRepo;
 
-    public Vendor[] getAllVdrs(){
+    public void smtVdr(Integer vdrid){
+        vdrRepo.smtVdr(vdrid);
+    }
+
+    public void delVdr(Integer vdrid){
+        vdrRepo.delVdr(vdrid);
+    }
+
+    public Vendor[] getAllVdrs() {
         return vdrRepo.getAllVdrs();
     }
 
-    public Vendor[] getVdrsWithoutDate(String s1, String s2, String s3, String s4, String s5, String s6, String s7, String state,
-                            String i1, String i2, String i3, String i4, String i5, String i6, String i7, String i8, String i9, String i10){
-        String addr = s1+ ("".equals(s2)?"":(" "+s2)) + ("".equals(s3)?"":(" "+s3));
-        String vdrplate = s4+("".equals(s5)?"":(" "+s5));
-        logger.info("vendor查询， 条件地址为"+addr+"，条件牌照为："+vdrplate);
-        logger.info("其他查询条件："+state+" "+i1+" "+i2);
-        return vdrRepo.getVdrsWithoutDate(addr,vdrplate,s6,s7,state,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10);
-    }
+    public Vendor[] getVdrs(String vdraddr1, String vdraddr2, String vdraddr3, String vdrplate1, String vdrplate2, String contact, String contactphone, String firstdate, String state, String itemlist) {
+//        logger.info("查询中：用户名为{},手机号码为{}, 岗位为{}, 状态为{}", username, phone, roleid, state);
 
-    public Vendor[] getVdrsWithDate(String s1, String s2, String s3, String s4, String s5, String s6, String s7, Date d1, Date d2, String state,
-                            String i1, String i2, String i3, String i4, String i5, String i6, String i7, String i8, String i9, String i10){
-        String addr = s1+ ("".equals(s2)?"":(" "+s2)) + ("".equals(s3)?"":(" "+s3));
-        String vdrplate = s4+("".equals(s5)?"":(" "+s5));
-        return vdrRepo.getVdrsWithDate(addr,vdrplate,s6,s7,d1,d2,state,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10);
+        List<Vendor> resultList = null;
+        Date date1 = null;
+        Date date2 = null;
+
+        Specification<Vendor> querySpecifi = new Specification<Vendor>() {
+
+            private static final long serialVersionUID = 1L;
+            String addr = vdraddr1 + ("".equals(vdraddr2) ? "" : (" " + vdraddr2)) + ("".equals(vdraddr3) ? "" : (" " + vdraddr3));
+            String vdrplate = vdrplate1 + ("".equals(vdrplate2) ? "" : (" " + vdrplate2));
+            String[] a = DataUtil.VdrSearchTrim(itemlist.split(","));
+
+            @Override
+            public Predicate toPredicate(Root<Vendor> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+
+                List<Predicate> predicates = new ArrayList<>();
+                if (StringUtils.isNotBlank(addr)) {
+                    predicates.add(criteriaBuilder.like(root.get("vdraddr"), "%" + addr + "%"));
+                }
+                if (StringUtils.isNotBlank(vdrplate)) {
+                    predicates.add(criteriaBuilder.like(root.get("vdrplate"), "%" + vdrplate + "%"));
+                }
+                if (StringUtils.isNotBlank(contact)) {
+                    predicates.add(criteriaBuilder.like(root.get("vdrplate"), "%" + contact + "%"));
+                }
+                if (StringUtils.isNotBlank(contactphone)) {
+                    predicates.add(criteriaBuilder.like(root.get("vdrplate"), "%" + contactphone + "%"));
+                }
+                if (StringUtils.isNotBlank(firstdate)) {
+                    String[] dates = firstdate.split(" ");
+                    Date date1 = DateUtil.StrToDate(dates[0]);
+                    Date date2 = DateUtil.StrToDate(dates[1]);
+                    predicates.add(criteriaBuilder.greaterThan(root.get("firstdate"), "%" + date1 + "%"));
+                    predicates.add(criteriaBuilder.lessThan(root.get("firstdate"), "%" + date2 + "%"));
+                }
+                if (StringUtils.isNotBlank(state)) {
+                    predicates.add(criteriaBuilder.like(root.get("state"), "%" + state + "%"));
+                }
+                if (StringUtils.isNotBlank(itemlist)) {
+                    predicates.add(criteriaBuilder.like(root.get("state"), "%" + state + "%"));
+                }
+                if (StringUtils.isNotBlank(a[0].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemTidang"), "%" + a[0] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[1].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemGuohu"), "%" + a[1] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[2].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemShangpai"), "%" + a[2] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[3].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemWeizhang"), "%" + a[3] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[4].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemDiya"), "%" + a[4] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[5].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemJiechudiya"), "%" + a[5] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[6].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemWeituo"), "%" + a[6] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[7].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemNianjian"), "%" + a[7] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[8].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemBuhuan"), "%" + a[8] + "%"));
+                }
+                if (StringUtils.isNotBlank(a[9].trim())) {
+                    predicates.add(criteriaBuilder.equal(root.get("itemQita"), "%" + a[9] + "%"));
+                }
+                return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+            }
+        };
+        resultList = this.vdrRepo.findAll(querySpecifi);
+        if (resultList != null && !resultList.isEmpty()) {
+            return resultList.toArray(new Vendor[]{});
+        }
+        return null;
     }
 
     public Integer createVdr(String creator) {
@@ -55,19 +135,19 @@ public class VendorService {
         return v.getVdrid();
     }
 
-    public void saveVdr(Vendor v){
+    public void saveVdr(Vendor v) {
         vdrRepo.save(v);
     }
 
-    public Vendor getVdr(Integer vdrid){
+    public Vendor getVdr(Integer vdrid) {
         return vdrRepo.getVdr(vdrid);
     }
-    
-    
-    public Page<Vendor> queryVendorPage(Vendor vendor,int currentPageIndex, int pageSize) {
-		Sort sort = new Sort(Sort.Direction.DESC,"vdrid");
-		Pageable pageable = PageRequest.of(currentPageIndex,pageSize,sort);
-    	return vdrRepo.findAll(pageable);
-	}    
-    
+
+
+    public Page<Vendor> queryVendorPage(Vendor vendor, int currentPageIndex, int pageSize) {
+        Sort sort = new Sort(Sort.Direction.DESC, "vdrid");
+        Pageable pageable = PageRequest.of(currentPageIndex, pageSize, sort);
+        return vdrRepo.findAll(pageable);
+    }
+
 }
